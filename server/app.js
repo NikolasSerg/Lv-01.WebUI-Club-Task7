e1184c3db7aad3c6f64981c905dea51e45162cdd
SERG <NIKOLASALON@GMAIL.COM>
2021-09-14 21:19:33 +0300

    the method of response was created

--- server/app.js
+++ server/app.js
@@ -5,8 +5,17 @@ const fs = require('fs');
 
 const PORT = 3000;
 
+app.get("/api/users", (req, res) => {
+    fs.readFile("userInfo.json", "utf8", (err, data) => {
+        if(err) {
+            res.status(500).send({message: "error loading file on server"})
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
 
 app.listen(PORT, (error) => {
-    if(error) console.error('Conection error')
+    if(error) console.error('Connection error')
     console.log(`server works on ${PORT} port`)
 })

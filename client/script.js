8e846a816d75506c0f9bdb09f1117347f72551a5
NotSimpleUser <kbshnik2020@gmail.com>
2021-09-16 19:38:09 +0300

    Created loading function

--- client/script.js
+++ client/script.js
@@ -1,47 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-<div class="container wrap">
-<header>
-    <nav class="indigo lighten-2">
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">STAFF</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#">Співробітники</a></li>
-                <li><a href="#">Стажери</a></li>
-                <li class="active"><a href="#">Резюме</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
-<main>
-     <div class="load">
-        <a class="waves-effect waves-light btn-large loadBtn">LOAD</a>
-        <h3 class="nonDisplay">LOADING ...</h3>
-    </div>
-    <div class="cardList"></div>
-</main>
+let loadBtn = document.querySelector('.loadBtn');
+let cardList = document.querySelector('.cardList');
+let load = document.querySelector('.load');
+let loading = document.querySelector('.load h3');
 
 
-<footer class="page-footer indigo lighten-2">
-        <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-        </div>
-</footer>
-</div>
+loadBtn.addEventListener('click',  () => {
+    loadBtn.classList.add('nonDisplay');
+    loading.className = ''
 
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script src="script.js"></script>
-</body>
-</html>
+    fetch("http://localhost:3000/api/users")
+        .then(res => res.json())
+        .then(data => {
+            load.className = 'nonDisplay';
+        })
+        .catch(error => alert('Error of loading data'))
+})
